8223b4ce934c5a78c7763e790ced0fd817095c66
GleidsonLM <gleidsonlm@gmail.com>
2022-11-01 16:44:40 -0300

    server: database modeling and erd completed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify, { fastify } from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from '@prisma/client';
 
 /* Logging Prisma on Console */
@@ -10,21 +11,26 @@ async function bootstrap() {
     /* starting Fastify server with prettyPrint logger */
     const fastify = Fastify({
         logger: true,
-        })
+    })
     
+    /* Fastify Cors register all origin in dev env. */
+    await fastify.register(cors, {
+        origin: true,
+    })
+
     /* Routes */
+    
     // GET /pools/count return the number of pools (pt-br: enquete | bolão)
-    fastify.get('/pools/count', async() => {
-        
-        const pools = prisma.pool.findMany({
-            where: { code: {startsWith: 'b' } }
-        })
-        
-        return { count: pools }
+    fastify.get('/pools/count', async() => { 
+    const count = await prisma.pool.count();
+    return { count }
     });
 
     /* listen on port 3333 */
-    await fastify.listen({ port: 3333 })
+    await fastify.listen({
+        port: 3333,
+        host: '0.0.0.0'
+    })
 }
 
 bootstrap();
