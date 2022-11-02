76dc56260552cdcebf6f4d6e319c4857c1a300f3
GleidsonLM <gleidsonlm@gmail.com>
2022-11-02 17:13:54 -0300

    add: status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Loading } from './src/components/Loading';
@@ -11,7 +11,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={themeCopa}>
+
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
       { fontsLoaded ? <SignIn /> : <Loading /> }
+    
     </NativeBaseProvider>
   );
 }
