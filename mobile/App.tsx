2dab4d1e508ff7d62fce96f5785335a4f8507db2
GleidsonLM <gleidsonlm@gmail.com>
2022-11-02 16:52:48 -0300

    add: check fonts load, roboto fonts working

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from "native-base";
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-/* todo: fix for theme.ts file name */
-import { THEME } from './src/styles/theme.js'
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+import { themeCopa } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor='gray.900' >
-        <Text color='white' fontSize={24} >NLW Copa</Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={themeCopa}>
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
