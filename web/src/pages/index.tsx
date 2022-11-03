83c1c4624a3b6816e2f25a114894384c17bf9dd6
GleidsonLM <gleidsonlm@gmail.com>
2022-11-03 15:43:11 -0300

    add: function to add pool from frontend through api promise

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import Image from 'next/image'
 import appPreviewImage from '../assets/app-preview.png';
 import LogoImage from '../assets/logo.svg'
@@ -13,6 +13,32 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+
+  /* useState to retrieve value from input */
+  const [poolTitle, setPoolTitle] = useState('')
+
+  /* Receives form inputs and parse them to function to create object with API POST /pools */
+  async function createPool(event: FormEvent) {
+    event.preventDefault();
+
+    /* TryCatch axios promise POST /pools returns either the code of created pool or error alert */
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+      const { code } = response.data;
+      await navigator.clipboard.writeText(code);
+      alert('\n\nI just copied the code to your clipboard.\n\n' + code + '\n\nYou can share it anywhere now.');
+      setPoolTitle('');
+    } catch (error) {
+      console.error(error)
+      alert('Ops, sorry. \n Please, try again.')
+    }
+
+  };
+
+
+
   return (
     <div className='max-w-5xl h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
@@ -27,10 +53,17 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
 
-        <form className='mt-10'>
+        <form onSubmit={createPool} className='mt-10'>
           <fieldset className='flex gap-2'>
             <legend className=' hidden'>After creating your group, you&apos;ll receive a code to invite your guests.</legend>
-            <input type='text' required placeholder="What's your group name?" className=' flex-1 px-6 py-4 rounded  bg-gray-800 border border-gray-600 text-sm' />
+            <input
+              onChange={event => setPoolTitle(event.target.value)}
+              value={poolTitle}
+              type='text'
+              placeholder="What's your group name?"
+              required
+              className=' flex-1 px-6 py-4 rounded  bg-gray-800 border border-gray-600 text-sm'
+            />
             <button type='submit' className=' bg-yellow-300 hover:bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase'>Create</button>
           </fieldset>
           <p className='mt-4 text-gray-300 place-self-end leading-relaxed' aria-disabled='false' >After creating your group, you&apos;ll receive a code to invite your friends, share your predictions and check the scoreboard. \uD83D\uDE80</p>
@@ -52,6 +85,10 @@ export default function Home(props: HomeProps) {
   )
 }
 
+/*
+  todo: Replace implementing the getStaticProps
+  https://nextjs.org/docs/basic-features/data-fetching/get-static-props
+ */
 export const getServerSideProps = async () => {
   const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
     await api.get('pools/count'),
