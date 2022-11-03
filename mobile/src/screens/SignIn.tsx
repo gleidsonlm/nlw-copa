f26cead3f6a8b4ce4d8b74742e81a449c6128ed4
GleidsonLM <gleidsonlm@gmail.com>
2022-11-03 16:43:34 -0300

    add: SVG NativeBaseSVG support and extending Button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,22 @@
-import { Text, Center } from 'native-base';
+import { Text, Center, Icon } from 'native-base';
+import { FontAwesome } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+
+import { Button } from '../components/Button'
 
 export function SignIn() {
     return (
-        <Center flex={1} bg='gray.900' >
-            <Text color='white' fontSize={24} fontFamily='heading' >SignIn</Text>
+        <Center flex={1} bgColor='gray.900' >
+            {/* <Text color='white' fontSize={24} fontFamily='heading' >SignIn</Text> */}
+            <Logo width={212} height={40} />
+
+            <Button
+                title='Sign in with Google'
+                leftIcon={
+                    <Icon as={FontAwesome} name="google" color="white" size="md" />
+                }
+            />
         </Center>
     )
 }
