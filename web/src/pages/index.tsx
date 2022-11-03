efd4eeda25cfd2fe722313c7353daa13d268adb3
GleidsonLM <gleidsonlm@gmail.com>
2022-11-03 14:53:37 -0300

    add: replace text with dynamic info retrieved from backend with api

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,20 +1,69 @@
+import React from 'react'
+import Image from 'next/image'
+import appPreviewImage from '../assets/app-preview.png';
+import LogoImage from '../assets/logo.svg'
+import usersAvatarExampleImage from '../assets/users-avatar-example.png'
+import iconCheckImage from '../assets/icon-check.svg'
+import { api } from '../../lib/axios';
+
 interface HomeProps {
-  count: number;
+  poolCount: number;
+  guessCount: number;
+  userCount: number;
 }
 
 export default function Home(props: HomeProps) {
   return (
-    <div>
-      <h1 className='text-4xl text-yellow-400'>Title 1 - </h1>
-      <p>{props.count} Pools created</p>
+    <div className='max-w-5xl h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
+      <main>
+        <Image src={LogoImage} alt="logo for the NLW Copa app" aria-hidden='true' />
+
+        <h1 className='mt-14 text-5xl text-white font-bold leading-tight'>Invite your friends to share predictions for the World Cup 2022 games!</h1>
+
+        <div className='mt-10 flex items-center gap-2'>
+          <Image src={usersAvatarExampleImage} alt="image showing user's avatar using the app" />
+          <strong className='text-xl'>
+            <span className=' text-ignite-500'>+{props.userCount}</span> friends playing predictions.
+          </strong>
+        </div>
+
+        <form className='mt-10'>
+          <fieldset className='flex gap-2'>
+            <legend className=' hidden'>After creating your group, you&apos;ll receive a code to invite your guests.</legend>
+            <input type='text' required placeholder="What's your group name?" className=' flex-1 px-6 py-4 rounded  bg-gray-800 border border-gray-600 text-sm' />
+            <button type='submit' className=' bg-yellow-300 hover:bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase'>Create</button>
+          </fieldset>
+          <p className='mt-4 text-gray-300 place-self-end leading-relaxed' aria-disabled='false' >After creating your group, you&apos;ll receive a code to invite your friends, share your predictions and check the scoreboard. \uD83D\uDE80</p>
+        </form>
+
+        <div className='pt-10 mt-10 border-t bt-gray-600 divide-x divide-gray-600  grid grid-cols-2'>
+          <div className='flex items-center gap-6 text-sm justify-start'>
+            <Image src={iconCheckImage} alt="decoration icon" aria-hidden='true' />
+            <span className='flex flex-col'><span className='text-bold text-2xl text-ignite-500'>+{props.poolCount}</span><span>&nbsp; groups created.</span></span>
+          </div>
+          <div className='flex items-center gap-6 text-sm justify-end'>
+            <Image src={iconCheckImage} alt="decoration icon" aria-hidden='true' />
+            <span className='flex flex-col'><span className='text-bold text-2xl text-ignite-500'>+{props.guessCount}</span><span>&nbsp; predictions shared.</span></span>
+          </div>
+        </div>
+      </main>
+      <Image src={appPreviewImage} alt="two mobile phones with app preview" />
     </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const response = await fetch('http://localhost:3333/pools/count')
-  const data = await response.json()
+  const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    await api.get('pools/count'),
+    await api.get('guesses/count'),
+    await api.get('guesses/count')
+  ])
+
   return {
-    props: { count: data.count }
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
+    }
   }
 }
