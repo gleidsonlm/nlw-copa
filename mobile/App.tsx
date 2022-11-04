607a0839850609da49041da0d68f79d586663e5f
GleidsonLM <gleidsonlm@gmail.com>
2022-11-04 18:48:02 -0300

    add: reused New.tsx to create Find.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Loading } from './src/components/Loading';
-import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 
 import { themeCopa } from './src/styles/theme';
 import { AuthContextProvider } from './src/contexts/AuthContexts';
@@ -19,7 +19,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded ? <New /> : <Loading /> }
+        { fontsLoaded ? <Find /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
