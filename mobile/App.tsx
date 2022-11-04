d839cec84b1ad5e821052633db177d616df3af83
GleidsonLM <gleidsonlm@gmail.com>
2022-11-04 18:04:11 -0300

    add: included components from nlwcopa-mobile-components, and started screens/New.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
 
 import { themeCopa } from './src/styles/theme';
 import { AuthContextProvider } from './src/contexts/AuthContexts';
@@ -19,7 +19,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <New /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
