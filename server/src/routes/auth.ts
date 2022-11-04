0d38ec533ef2f2d84ceac9b7b7c8b942b0364894
GleidsonLM <gleidsonlm@gmail.com>
2022-11-04 11:16:10 -0300

    fix: prisma import from /lib/prisma.ts on auth.ts

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@prisma/client';
+import { prisma } from '../lib/prisma'
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { authenticate } from '../plugins/authenticate';
