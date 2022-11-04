68f22fd81cc5e8e522d553d5d0346fb8b1fb8a68
GleidsonLM <gleidsonlm@gmail.com>
2022-11-04 15:17:12 -0300

    web: started routes game pools/:id/games

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,3 +1,21 @@
+import { prisma } from '../lib/prisma'
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { authenticate } from '../plugins/authenticate';
 
-export async function gameRoutes(fastify: FastifyInstance){};
+export async function gameRoutes(fastify: FastifyInstance){
+    // GET /pools/:id/games return user guesses in the pools.
+    fastify.get('/pools/:id/games',
+        { onRequest: [authenticate] },
+        async (request, reply) => {
+        
+        // validate request params
+        const getPoolParams = z.object({ id: z.string() });
+        // parse request parameters to validate function    
+        const { id } = getPoolParams.parse(request.params);
+
+        const games = await prisma.game.findMany({
+            where: {}
+        });
+    });
+};
