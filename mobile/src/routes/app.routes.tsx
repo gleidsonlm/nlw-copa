2cffa0fdd558887ed375e344a9558ece671bfb3a
GleidsonLM <gleidsonlm@gmail.com>
2022-11-05 00:11:55 -0300

    add: navigation tab icons styles

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,22 +1,52 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { New } from '../screens/New';
 import { Pools } from '../screens/Pools';
-
+import { PlusCircle, SoccerBall } from 'phosphor-react-native';
+import { useTheme } from 'native-base';
+import { Platform } from 'react-native';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+    // use colors from react native theme
+    const { colors, sizes } = useTheme();
+    // standarize icon size
+    const iconSize = sizes[8];
     return (
-    <Navigator>
+    <Navigator screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarActiveTintColor: colors.yellow[500],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarStyle: {
+            position: 'absolute',
+            height: sizes[22],
+            borderTopWidth: 0,
+            backgroundColor: colors.gray[800],
+        },
+        tabBarItemStyle: {
+            position: 'relative',
+            top: Platform.OS === 'android' ? -10 : 0,
+        }
+    }}
+    >
 
         <Screen
             name="new"
             component={New}
+            options={{ 
+                tabBarIcon: ({color}) => <PlusCircle color={color} size={iconSize} />,
+                tabBarLabel: 'Create Group',
+            }}
         />
 
         <Screen
             name="pools"
             component={Pools}
+            options={{ 
+                tabBarIcon: ({color}) => <SoccerBall color={color} size={iconSize}/>, 
+                tabBarLabel: 'My Groups',
+            }}
         />
     </Navigator>    
     )
