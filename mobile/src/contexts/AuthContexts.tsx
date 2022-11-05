e63f062f04e7f48ea78e8661a717d3257b467215
GleidsonLM <gleidsonlm@gmail.com>
2022-11-05 12:40:47 -0300

    fix: mobile types reflecting fixes in the server backend

--- mobile/src/contexts/AuthContexts.tsx
+++ mobile/src/contexts/AuthContexts.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useEffect, useState } from 'react';
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from '../services/api'
 
 
 interface UserProps {
@@ -47,6 +48,20 @@ export function AuthContextProvider({ children }:AuthProviderProps){
 
     async function signInWithGoogle (access_token: string) {
         console.log(access_token);
+        try {
+            setIsUserLoading(true);
+
+            const tokenResponse = await api.post('/users',{ access_token });
+            console.log(tokenResponse.data.token)
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+
+        } finally {
+            setIsUserLoading(false);
+
+        }
     }
 
     // useEffect will listen to Google.response
