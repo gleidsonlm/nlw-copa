0e018e0020f70b112b22a7d19b3bedc0475ffe54
GleidsonLM <gleidsonlm@gmail.com>
2022-11-04 23:43:34 -0300

    add: implemented react navigation bottom tab and added first routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,9 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
 import { Loading } from './src/components/Loading';
-import { Pools } from './src/screens/Pools';
-
 import { themeCopa } from './src/styles/theme';
 import { AuthContextProvider } from './src/contexts/AuthContexts';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -19,7 +17,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded ? <Pools /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
