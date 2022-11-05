aa6d857b67f1d70c1154dc687a4f3a64ac77a3cd
GleidsonLM <gleidsonlm@gmail.com>
2022-11-05 17:34:20 -0300

    add: mobile pools screen, its navigation and api functions

--- mobile/src/components/EmptyPoolList.tsx
+++ mobile/src/components/EmptyPoolList.tsx
@@ -1,29 +1,32 @@
+import { useNavigation } from '@react-navigation/native';
 import { Row, Text, Pressable } from 'native-base';
 
 export function EmptyPoolList() {
+  const { navigate } = useNavigation();
+
   return (
     <Row flexWrap="wrap" justifyContent="center">
-      <Text color="white" fontSize="sm" textAlign="center">
-        Você ainda não está participando de {'\n'} nenhum bolão, que tal
+      <Text color="white" fontSize="md" textAlign="center">
+        How about to {' '}
       </Text>
 
-      <Pressable>
-          <Text textDecorationLine="underline" color="yellow.500" textDecoration="underline">
-            buscar um por código
+      <Pressable onPress={() => navigate('find')}>
+          <Text fontSize="md" textDecorationLine="underline" color="yellow.500" textDecoration="underline">
+          use a code 
           </Text>
       </Pressable>
 
-      <Text color="white" fontSize="sm" textAlign="center" mx={1}>
-        ou
+      <Text color="white" fontSize="md" textAlign="center" >
+        {' '} that someone did send you, or {' '}
       </Text>
 
-      <Pressable>
-        <Text textDecorationLine="underline"  color="yellow.500">
-          criar um novo
+      <Pressable onPress={() => navigate('new')}>
+        <Text fontSize="md" textDecorationLine="underline"  color="yellow.500">
+          create a new group
         </Text>
       </Pressable>
 
-      <Text color="white" fontSize="sm" textAlign="center">
+      <Text color="white" fontSize="md" textAlign="center">
         ?
       </Text>
     </Row>
--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,11 +1,54 @@
-import { VStack, Icon } from "native-base";
+import { useCallback, useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { FontAwesome } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+
 import { Header } from "../components/Header";
 import { Button } from "../components/Button";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+
+import { api } from "../services/api";
 
 export function Pools() {
+    // useState for storing PoolCards objects in an array
+    const [pools, setPools] = useState<PoolCardProps[]>([]);
+    // useState for check if is loading
+    const [isLoading, setIsLoading] = useState(true);
+    // useToast will deliver messages for the UI
+    const toast = useToast();
+    // useNavigation bottom tabs
     const { navigate } = useNavigation();
+
+    // request Pools from authenticated user 
+    async function fetchPools() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/pools');
+            setPools(response.data);
+        } catch (error) {
+            console.log(error)
+            toast.show({
+                title: "Sorry, couldn't get your groups. Please, try again.",
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    // callback function when screen is on focus
+    useFocusEffect(useCallback(() => {
+        fetchPools();
+    },[]))
+
+
+/*     useEffect(() => {
+        fetchPools();
+    },[]);
+ */    
 
     return (
         <VStack flex={1} bgColor="gray.900">
@@ -20,6 +63,18 @@ export function Pools() {
                     onPress={() => navigate('find')}
                 />
             </VStack>
+
+            { isLoading ? <Loading /> :
+                <FlatList
+                data={pools}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <PoolCard data={item} />}
+                px={5}
+                showsVerticalScrollIndicator={false}
+                _contentContainerStyle={{ pb: 10 }}
+                ListEmptyComponent={() => <EmptyPoolList />}
+                />
+             }
 
         </VStack>
     );
